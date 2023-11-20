866ee362aecb1ebb4967325c5f1bc9dce33de458
AnnYaniv <yaniv.anya@gmail.com>
2023-11-20 21:57:31 +0100

    Add inmemory-spring-boot-starter

--- inmemory-spring-boot-starter/src/main/java/com/andersenlab/inmemoryspringbootstarter/InMemoryAutoConfiguration.java
+++ inmemory-spring-boot-starter/src/main/java/com/andersenlab/inmemoryspringbootstarter/InMemoryAutoConfiguration.java
@@ -1,4 +1,4 @@
-package com.andersenlab.springinterface.config;
+package com.andersenlab.inmemoryspringbootstarter;
 
 import com.andersenlab.hotel.model.Apartment;
 import com.andersenlab.hotel.model.ApartmentSort;
@@ -7,23 +7,17 @@ import com.andersenlab.hotel.model.ClientSort;
 import com.andersenlab.hotel.repository.SortableCrudRepository;
 import com.andersenlab.hotel.repository.inmemory.InMemoryApartmentRepository;
 import com.andersenlab.hotel.repository.inmemory.InMemoryClientRepository;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
-
-//TODO change this to starters
 @Configuration
-public class RepositoriesConfiguration {
+public class InMemoryAutoConfiguration {
     @Bean
-    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
     public SortableCrudRepository<Client, ClientSort> clientRepository() {
         return new InMemoryClientRepository();
     }
 
     @Bean
-    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
     public SortableCrudRepository<Apartment, ApartmentSort> apartmentRepository() {
         return new InMemoryApartmentRepository();
     }
